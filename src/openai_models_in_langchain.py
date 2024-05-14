797bb9f05be0f7070d36907abab518e1efdd28dd
datttrian <datttrian@gmail.com>
2024-05-14 18:33:56 +0000

    Tue, May 14, 2024, 6:33 PM +00:00

---
+++ src/openai_models_in_langchain.py
@@ -0,0 +1,19 @@
+import os
+
+import openai
+from dotenv import load_dotenv
+from langchain_openai import OpenAI
+
+load_dotenv()
+
+
+# Set your API Key from OpenAI
+openai.api_key = os.environ["OPENAI_API_KEY"]
+# Define the LLM
+llm = OpenAI(model_name="gpt-3.5-turbo-instruct")  # type: ignore
+
+# Predict the words following the text in question
+question = "Whatever you do, take care of your shoes"
+output = llm.invoke(question)
+
+print(output)
